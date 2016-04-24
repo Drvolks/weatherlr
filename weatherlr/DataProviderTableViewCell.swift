f8b82332351cf9eceedce8b1e869b94a73462c6f
Jean-Francois Dufour <jfdufour@me.com>
2016-04-24 11:52:03 -0400

    ajout info environement canada

---
+++ weatherlr/DataProviderTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  DataProviderTableViewCell.swift
+//  weatherlr
+//
+//  Created by Jean-Francois Dufour on 2016-04-24.
+//  Copyright © 2016 Jean-Francois Dufour. All rights reserved.
+//
+
+import UIKit
+
+class DataProviderTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var dataProviderLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

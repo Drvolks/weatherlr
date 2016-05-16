7d433fa33eac4656369bae69d81448a5e79af232
Jean-Francois Dufour <jfdufour@me.com>
2016-05-16 16:47:22 -0400

    nettoyer vue détail alerte

--- weatherlr/AlertDetailViewController.swift
+++ weatherlr/AlertDetailViewController.swift
@@ -25,7 +25,9 @@ class AlertDetailViewController: UIViewController, UIWebViewDelegate {
     }
     
     func webViewDidFinishLoad(webView: UIWebView) {
-        
+        // followus hidden-print
+        let js = "document.getElementById('wb-bc').remove(); document.getElementsByClassName('followus hidden-print')[0].remove(); document.getElementsByClassName('container hidden-print')[0].remove(); document.getElementById('weather-topics').remove(); document.getElementsByClassName('row pagedetails')[0].remove(); document.getElementsByClassName('gc-nttvs container')[0].remove(); document.getElementById('wb-info').remove();"
+        webView.stringByEvaluatingJavaScriptFromString(js)
     }
     
     @IBAction func cancel(sender: UIBarButtonItem) {

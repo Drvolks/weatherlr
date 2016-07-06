b519e8b2bb86ccb79c65b989593092859a04fd19
drvolks <5374619+drvolks@users.noreply.github.com>
2016-07-06 17:48:11 -0400

    watch: min max affiché sur 2eme météo si pas de weather .Now

--- watch Extension/NextWeatherRowController.swift
+++ watch Extension/NextWeatherRowController.swift
@@ -14,13 +14,22 @@ class NextWeatherRowController : NSObject {
     @IBOutlet var detailLabel: WKInterfaceLabel!
     @IBOutlet var weatherImage: WKInterfaceImage!
     @IBOutlet var detailLine2Label: WKInterfaceLabel!
+    @IBOutlet var minMaxImage: WKInterfaceImage!
     
+    var previousWeatherPresent = true
     var weather:WeatherInformation? {
         didSet {
             if let weather = weather {
                 weatherLabel.setText(WeatherHelper.getWeatherDayWhenText(weather))
                 weatherImage.setImage(weather.image())
                 detailLabel.setText(weather.detail)
+                
+                if previousWeatherPresent {
+                    minMaxImage.setHidden(true)
+                } else {
+                    minMaxImage.setHidden(false)
+                    minMaxImage.setImage(WeatherHelper.textToImageMinMax(weather))
+                }
                 
                 if let range = weather.detail.rangeOfString(".") {
                     let line1 = String(weather.detail.characters.prefixUpTo(range.startIndex)).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())

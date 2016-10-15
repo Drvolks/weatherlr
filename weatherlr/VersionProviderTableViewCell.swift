27b153cc27f4e1fa19e59a0ee912f8ccd451ef95
Jean-Francois Dufour <jfdufour@me.com>
2016-10-15 10:06:29 -0400

    Afficher la version dans les settings

---
+++ weatherlr/VersionProviderTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  VersionProviderViewCell.swift
+//  weatherlr
+//
+//  Created by Jean-Francois Dufour on 16-10-15.
+//  Copyright © 2016 Jean-Francois Dufour. All rights reserved.
+//
+
+import UIKit
+
+class VersionProviderTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var versionLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}

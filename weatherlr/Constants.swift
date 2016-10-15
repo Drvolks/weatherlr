861545905bf61cd64e4b7435e21f0ade18506b9a
drvolks <5374619+drvolks@users.noreply.github.com>
2016-10-15 16:36:46 -0400

    bugs watch pour app gratuite

--- weatherlr/Constants.swift
+++ weatherlr/Constants.swift
@@ -19,7 +19,11 @@ class Constants {
     static let weatherCacheInMinutes = 30
     static let watchExpirationInMinutes = 240
     static let backgroundRefreshInSeconds = 30.0 * 60.0
-    static let backgroundDownloadTaskName = "BUNDLE_ID_PREFIX.weatherlr.download"
+    #if FREE
+        static let backgroundDownloadTaskName = "BUNDLE_ID_PREFIX.weatherlr.free.download"
+    #else
+        static let backgroundDownloadTaskName = "BUNDLE_ID_PREFIX.weatherlr.download"
+    #endif
     static let SettingGroup = "APP_GROUP_ID"
     static let googleAddId = "ca-app-pub-2793046476751764/6255610730"
     

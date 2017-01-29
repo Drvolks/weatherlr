746bb526fa04e1092bce44f2232fed25263ffa70
drvolks <5374619+drvolks@users.noreply.github.com>
2017-01-29 17:02:12 -0500

    Faire fonctionner  quick actions

--- weatherlr/AppDelegate.swift
+++ weatherlr/AppDelegate.swift
@@ -15,27 +15,47 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var shortcutItem: UIApplicationShortcutItem?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        print("Application did finish launching with options")
         
         #if FREE
             FIRApp.configure()
             GADMobileAds.configure(withApplicationID: Constants.googleAddId);
         #endif
         
-        return true
+        var performShortcutDelegate = true
+        
+        if let shortcutItem = launchOptions?[UIApplicationLaunchOptionsKey.shortcutItem] as? UIApplicationShortcutItem {
+            self.shortcutItem = shortcutItem
+            
+            performShortcutDelegate = false
+        }
+        
+        return performShortcutDelegate
     }
     
     func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Swift.Void) {
+        print("Application performActionForShortcutItem")
+        
         completionHandler(handleQuickAction(application: application, shortcutItem: shortcutItem))
     }
     
     func handleQuickAction(application: UIApplication, shortcutItem: UIApplicationShortcutItem) -> Bool {
+        print("Handling shortcut")
+        
         let index = shortcutItem.type.range(of: ":")?.upperBound
         let cityId = shortcutItem.type.substring(from: index!)
         
         PreferenceHelper.switchFavoriteCity(cityId: cityId)
+        
+        let mainSB = UIStoryboard(name: "Main", bundle: nil)
+        let viewController = mainSB.instantiateViewController(withIdentifier: "WeatherView") as! WeatherViewController
+        
+        let navVC = self.window?.rootViewController as! UINavigationController
+        navVC.pushViewController(viewController, animated: true)
         
         return true
     }
@@ -56,7 +76,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print("Application did become active")
+        
+        guard let shortcut = shortcutItem else { return }
+        
+        print("- Shortcut property has been set")
+        
+        handleQuickAction(application: application, shortcutItem: shortcut)
+        
+        self.shortcutItem = nil
     }
 
     func applicationWillTerminate(_ application: UIApplication) {

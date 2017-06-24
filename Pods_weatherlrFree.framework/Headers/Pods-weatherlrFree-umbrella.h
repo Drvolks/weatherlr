3a413400a59ddaccf8ee20f6120ee804cdda101b
Jean-François Dufour <jfdufour@me.com>
2017-06-24 13:13:29 -0400

    Refresh des Pods pour iOS11

--- Pods_weatherlrFree.framework/Headers/Pods-weatherlrFree-umbrella.h
+++ Pods_weatherlrFree.framework/Headers/Pods-weatherlrFree-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_watch_ExtensionVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_watch_ExtensionVersionString[];
+FOUNDATION_EXPORT double Pods_weatherlrFreeVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_weatherlrFreeVersionString[];
 

dc9a612469d8d8720bc55156ef87625c7b6e4ba3
drvolks <5374619+drvolks@users.noreply.github.com>
2017-09-02 12:20:18 -0400

    Corriger bug préférences sauvegardées avec utilisation commune de l'app normale et free

---
+++ weatherlr/PreferenceHelperError.swift
@@ -0,0 +1,13 @@
+//
+//  PreferenceHelperError.swift
+//  weatherlr
+//
+//  Created by drvolks on 17-09-02.
+//  Copyright © 2017 drvolks. All rights reserved.
+//
+
+import Foundation
+
+enum PreferenceHelperError: Error {
+    case unarchiveError
+}

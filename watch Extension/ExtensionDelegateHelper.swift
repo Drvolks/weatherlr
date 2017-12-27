a46b87c61d169ff4c38422e7b6739f5664ea80a8
Jean-Francois Dufour <jfdufour@me.com>
2017-12-27 14:14:31 -0500

    Refact background refresh watch

--- watch Extension/ExtensionDelegateHelper.swift
+++ watch Extension/ExtensionDelegateHelper.swift
@@ -10,13 +10,22 @@ import Foundation
 import WatchKit
 
 class ExtensionDelegateHelper {
-    static func launchURLSession() {
-        guard let delegate = WKExtension.shared().delegate as? ExtensionDelegate else {
-            print("launchURLSession: no delegate!")
-            return
-        }
+    static func launchURLSessionNow(_ delegate: URLSessionDelegate) {
+        #if DEBUG
+            print("launchURLSessionNow")
+        #endif
         
-        delegate.launchURLSession()
+        if let city = PreferenceHelper.getSelectedCity() {
+            let url = URL(string:UrlHelper.getUrl(city))!
+            
+            let configObject = URLSessionConfiguration.default
+            let session = URLSession(configuration: configObject, delegate: delegate, delegateQueue:nil)
+            
+            let downloadTask = session.downloadTask(with: url)
+            downloadTask.resume()
+        } else {
+            print("scheduleURLSession - no selected city")
+        }
     }
     
     static func refreshNeeded() -> Bool {
@@ -35,5 +44,23 @@ class ExtensionDelegateHelper {
         }
         
         delegate.wrapper = WeatherInformationWrapper()
+    }
+    
+    static func getWrapper() -> WeatherInformationWrapper {
+        guard let delegate = WKExtension.shared().delegate as? ExtensionDelegate else {
+            print("getWrapper: no delegate!")
+            return WeatherInformationWrapper()
+        }
+        
+        return delegate.wrapper
+    }
+    
+    static func setWrapper(_ wrapper: WeatherInformationWrapper) {
+        guard let delegate = WKExtension.shared().delegate as? ExtensionDelegate else {
+            print("getWrapper: no delegate!")
+            return
+        }
+        
+        return delegate.wrapper = wrapper
     }
 }

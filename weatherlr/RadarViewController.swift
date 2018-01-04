af7f0815c3f210cf19d43a6f7989f2be1b986b59
Jean-Francois Dufour <jfdufour@me.com>
2018-01-04 15:41:28 -0500

    passage à wkwebview

--- weatherlr/RadarViewController.swift
+++ weatherlr/RadarViewController.swift
@@ -7,17 +7,19 @@
 //
 
 import UIKit
+import WebKit
 
-class RadarViewController: UIViewController, UIWebViewDelegate {
-    @IBOutlet weak var webView: UIWebView!
+class RadarViewController: UIViewController, WKNavigationDelegate {
+    
+    @IBOutlet weak var webView: WKWebView!
     
     var city:City?
 
     override func viewDidLoad() {
-        webView.delegate = self
+        webView.navigationDelegate = self
         
         let url = UrlHelper.getRadarUrl(city!)
-        webView.loadRequest(URLRequest(url: URL(string: url)!))
+        webView.load(URLRequest(url: URL(string: url)!))
         
         super.viewDidLoad()
         
@@ -27,10 +29,10 @@ class RadarViewController: UIViewController, UIWebViewDelegate {
         self.title = "Radar".localized()
     }
     
-    func webViewDidFinishLoad(_ webView: UIWebView) {
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         // wb-sm
         let js = "document.body.style.backgroundColor = '#1f4f74'; document.getElementById('wb-cont').style.color = 'white'; document.getElementById('wb-bc').remove(); document.getElementById('wb-glb-mn').remove(); document.getElementsByClassName('container hidden-print')[0].remove(); document.getElementById('links').remove(); document.getElementById('weather-topics').remove(); document.getElementsByClassName('row pagedetails')[0].remove(); document.getElementsByClassName('gc-nttvs container')[0].remove(); document.getElementById('wb-info').remove(); document.getElementById('wb-sm').remove(); document.getElementsByClassName('well')[0].remove(); document.getElementById('wb-srch').remove();document.getElementById('wxo-overlay-cities').parentNode.parentNode.parentNode.parentNode.style.display = 'none'; document.getElementsByClassName('gc-prtts')[0].remove();"
-        webView.stringByEvaluatingJavaScript(from: js)
+        webView.evaluateJavaScript(js, completionHandler: { (html: AnyObject?, error: NSError?) in } as? (Any?, Error?) -> Void)
     }
 
     override func didReceiveMemoryWarning() {

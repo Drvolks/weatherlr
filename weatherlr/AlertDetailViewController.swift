a5b6915bdf5e61dd89bf811cfc723550721608c1
drvolks <5374619+drvolks@users.noreply.github.com>
2018-06-30 18:23:28 -0400

    version

--- weatherlr/AlertDetailViewController.swift
+++ weatherlr/AlertDetailViewController.swift
@@ -25,8 +25,9 @@ class AlertDetailViewController: UIViewController, UIWebViewDelegate {
     }
     
     func webViewDidFinishLoad(_ webView: UIWebView) {
-        let js = "document.getElementById('wb-bc').remove(); document.getElementById('wb-glb-mn').remove(); document.getElementsByClassName('followus hidden-print')[0].remove(); document.getElementsByClassName('container hidden-print')[0].remove(); document.getElementById('weather-topics').remove(); document.getElementsByClassName('row pagedetails')[0].remove(); document.getElementsByClassName('gc-nttvs container')[0].remove(); document.getElementById('wb-info').remove(); document.getElementById('wb-sm').remove(); document.getElementById('wb-srch').remove();" 
+        let js = "document.getElementById('wb-bc').remove(); document.getElementById('wb-glb-mn').remove(); document.getElementsByClassName('followus hidden-print')[0].remove(); document.getElementsByClassName('container hidden-print')[0].remove(); document.getElementById('weather-topics').remove(); document.getElementsByClassName('row pagedetails')[0].remove();  document.getElementById('wb-info').remove(); document.getElementById('wb-sm').remove(); document.getElementById('wb-srch').remove();"
         webView.stringByEvaluatingJavaScript(from: js)
+        // document.getElementsByClassName('gc-nttvs container')[0].remove();
     }
     
     @IBAction func cancel(_ sender: UIBarButtonItem) {

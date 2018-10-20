9eaf26d123106d86123f919cb39606f29cbec373
Jean-Francois Dufour <jfdufour@me.com>
2018-10-20 08:53:24 -0400

    CLKComplicationTemplateGraphicCircula

--- watch Extension/Complications/ComplicationTemplate.swift
+++ watch Extension/Complications/ComplicationTemplate.swift
@@ -16,7 +16,48 @@ protocol ComplicationTemplateProtocol {
 }
 
 open class ComplicationTemplate {
-    func temp(weather: WeatherInformation) -> String {
+    func temp(_ weather: WeatherInformation) -> String {
         return String(weather.temperature) + "°"
+    }
+    
+    func isDown(_ nextWeather:WeatherInformation) -> Bool {
+        var down = false
+        if nextWeather.tendancy == Tendency.minimum {
+            down = true
+        } else if nextWeather.tendancy == Tendency.steady {
+            if nextWeather.night {
+                down = true
+            }
+        }
+        
+        return down
+    }
+    
+    func gaugeFraction(down: Bool, weather: WeatherInformation, nextWeather: WeatherInformation) -> Float {
+        var min = 0
+        var max = 0
+        
+        if down {
+            min = nextWeather.temperature
+            max = weather.temperature + 10
+        } else {
+            min = weather.temperature - 10
+            max = nextWeather.temperature
+        }
+        
+        var fraction = Float(weather.temperature - min) / Float(max - min)
+        
+        // protection
+        if fraction > 1 {
+            fraction = Float(1)
+        } else if fraction < 0 {
+            fraction = Float(0)
+        }
+        
+        return fraction
+    }
+    
+    func ringColor() -> UIColor {
+        return UIColor(weatherColor:WeatherColor.watchRing)
     }
 }
--- watch Extension/Complications/ComplicationTemplateGraphicCircular.swift
+++ watch Extension/Complications/ComplicationTemplateGraphicCircular.swift
@@ -11,26 +11,43 @@ import WatchKit
 
 class ComplicationTemplateGraphicCircular: ComplicationTemplate, ComplicationTemplateProtocol {
     func generate(_ weather: WeatherInformation?, nextWeather: WeatherInformation?, city: City) -> CLKComplicationTemplate {
-        let modularTemplate = initialState() as! CLKComplicationTemplateGraphicCircularImage
+        let modularTemplate = initialState() as! CLKComplicationTemplateGraphicCircularOpenGaugeRangeText
         
         if let weather = weather {
-            modularTemplate.imageProvider = WatchImageHelper.getImageProviderFull(weatherInformation: weather)
+            if let nextWeather = nextWeather {
+                let down = isDown(nextWeather)
             
+                if(down) {
+                    modularTemplate.leadingTextProvider = CLKSimpleTextProvider(text: temp(nextWeather))
+                } else {
+                    modularTemplate.trailingTextProvider = CLKSimpleTextProvider(text: temp(nextWeather))
+                }
+                
+                let fraction = gaugeFraction(down: down, weather: weather, nextWeather: nextWeather)
+                modularTemplate.gaugeProvider = CLKSimpleGaugeProvider(style: .ring, gaugeColor: ringColor(), fillFraction: fraction)
+            }
+            
+            modularTemplate.centerTextProvider = CLKSimpleTextProvider(text: temp(weather))
         }
         
         return modularTemplate
     }
     
     func initialState() -> CLKComplicationTemplate {
-        let modularTemplate = CLKComplicationTemplateGraphicCircularImage()
-        // TODO avoir une image plus générique
-        modularTemplate.imageProvider = CLKFullColorImageProvider(fullColorImage: UIImage(named: "ComplicationSunny")!)
+        let modularTemplate = CLKComplicationTemplateGraphicCircularOpenGaugeRangeText()
+        modularTemplate.centerTextProvider = CLKSimpleTextProvider(text: "25°")
+        modularTemplate.leadingTextProvider = CLKSimpleTextProvider(text: "")
+        modularTemplate.trailingTextProvider = CLKSimpleTextProvider(text: "30")
+        modularTemplate.gaugeProvider = CLKSimpleGaugeProvider(style: .fill, gaugeColor: ringColor(), fillFraction: 0.6)
         return modularTemplate
     }
     
     func demoState() -> CLKComplicationTemplate {
-        let modularTemplate = CLKComplicationTemplateGraphicCircularImage()
-        modularTemplate.imageProvider = CLKFullColorImageProvider(fullColorImage: UIImage(named: "ComplicationSunny")!)
+        let modularTemplate = CLKComplicationTemplateGraphicCircularOpenGaugeRangeText()
+        modularTemplate.centerTextProvider = CLKSimpleTextProvider(text: "--")
+        modularTemplate.leadingTextProvider = CLKSimpleTextProvider(text:"")
+        modularTemplate.trailingTextProvider = CLKSimpleTextProvider(text:"")
+        modularTemplate.gaugeProvider = CLKSimpleGaugeProvider(style: .fill, gaugeColor: ringColor(), fillFraction: 0)
         return modularTemplate
     }
 }

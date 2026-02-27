82f450f76d8c10864bf1657c6e05b21e8d6f7d17
drvolks <5374619+drvolks@users.noreply.github.com>
2026-02-27 15:42:22 -0500

    Pre-load radar data

--- weatherlr/WMSTileOverlay.swift
+++ weatherlr/WMSTileOverlay.swift
@@ -27,6 +27,24 @@ class WMSTileOverlay: MKTileOverlay {
         return URL(string: urlString)!
     }
 
+    override func loadTile(at path: MKTileOverlayPath, result: @escaping (Data?, Error?) -> Void) {
+        let tileURL = url(forTilePath: path)
+        let request = URLRequest(url: tileURL)
+
+        if let cached = URLCache.shared.cachedResponse(for: request) {
+            result(cached.data, nil)
+            return
+        }
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data, let response = response {
+                let cachedResponse = CachedURLResponse(response: response, data: data)
+                URLCache.shared.storeCachedResponse(cachedResponse, for: request)
+            }
+            result(data, error)
+        }.resume()
+    }
+
     private func tileBBox(x: Int, y: Int, z: Int) -> (minX: Double, minY: Double, maxX: Double, maxY: Double) {
         let tileSize = (2 * WMSTileOverlay.originShift) / pow(2.0, Double(z))
         let minX = Double(x) * tileSize - WMSTileOverlay.originShift
